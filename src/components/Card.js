764921a627064692b7b046e58a845621848f5492
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-04-25 23:19:57 -0700

    Working on making the cards flip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,36 @@
-// import React from 'react';
-// import './Card.css';
-// import { useState } from 'react';
-// import card from '../images/card.png';
+import React from 'react';
+import './Card.css';
+import { useState } from 'react';
+import card from '../images/card.png';
 
 
-// export default function Card(props) {
-//     return(
-//         <div className='card-container'>
-//             HELLLOOOO
-//         </div>
-//     )
-// }
+export default function Card(props) {
+
+    const [logoIsHovered, setLogoIsHovered] = useState(false);
+
+
+    function hoverHandler(){
+        console.log('Hovered!');
+        setLogoIsHovered(true);
+    }
+    function hoverHandler2(){
+        console.log('Un Hovered');
+        setLogoIsHovered(false);
+    }
+
+    return(
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img className = 'card1' src={props.img1} alt=""/>
+                </div>
+            <div class="flip-card-back">
+                <h1>John Doe</h1>
+                <p>We love that guy</p>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+{/* <img className='card1' src={props.img1} alt=''/> */}
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Project.css';
 // import { useState } from 'react';
 import card from '../images/card.png';
+import card2 from '../images/card2.png';
+import card3 from '../images/card3.png';
+import card4 from '../images/card4.png';
+import Card from './Card';
 
 export default function Project(){
     return(
@@ -10,10 +14,13 @@ export default function Project(){
                 Projects
             </h1>
             <div className='cards-container'>
-                <img className='card' src={card} atl='card'/>
-                <img className='card' src={card} atl='card'/>
-                <img className='card' src={card} atl='card'/>
-                <img className='card' src={card} atl='card'/>
+                <Card img1={card} img2={card2} />
+                {/* Card component accepts two images. Card component flips them */}
+                {/* https://stackoverflow.com/questions/52999377/react-passing-import-name-as-prop-and-adding-to-image-src */}
+                <img className='card' src={card} alt='card'/>
+                <img className='card' src={card2} alt='card2'/>
+                <img className='card' src={card3} alt='card3'/>
+                <img className='card' src={card4} alt='card4'/>
             </div>
         </div>
     )
