76d2948e4eaa5e31a5250789744199d4028aca54
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-06-06 20:00:29 -0700

    about text looks good on mobile and desk
    now to add the about header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,11 +9,16 @@ function Hero(){
 
         <div className={classes.about_container}>
             <img className={classes.pic} src={yo} alt=''/>
-            <p className={classes.text}>Hello my name is Jorge. This is an about me section
-                I'm going to put a lot of stuff here. Possible like 
-                one or two paragraphs, not sure atm. It'll be a bunch
-                of boring stuff like what I like to do in my spare time.
-            </p>
+            <div className={classes.text}>
+                <h1 className={classes.h}>Jorge Mejia</h1>
+                {/* <hr className={classes.line}></hr> */}
+                <p className={classes.summ}>
+                    Hello my name is Jorge. This is an about me section
+                    I'm going to put a lot of stuff here. Possible like 
+                    one or two paragraphs, not sure atm. It'll be a bunch
+                    of boring stuff like what I like to do in my spare time.
+                </p>
+            </div>
         </div>
     )
 }
